ca8c0978939e1ed847ff55952757304305d2a9a1
IvanDimanov <IvanDimanov1@gmail.com>
2012-04-19 18:07:30 +0300

    create /jsonapi/newbadges create onClode() function for the common_pop_up.html update the onHash() global function

--- kit/js/controllers.js
+++ kit/js/controllers.js
@@ -2,21 +2,76 @@
 
 
 // Global hash change callback register
-window.onHash = [];
+window.hashes = {};
+
+
+// Add a callback function to a specific hash
+window.onHash = function (hash, callback) {
+  // if this is the first instance for this hash we'll create a ready-to-use arrays to be filled with callback functions
+  if (!window.hashes[hash]) {
+    window.hashes[hash] = new Array();
+  }
+  
+  // Push the income callback function to its specific hash key
+  window.hashes[hash].push(callback);
+};
+
+
+// Run all callback functions (if any) for a specific hash
+// A parameter hash functionality is added
+// Example:
+//    if we want to run a specific event for a strict hash we use
+//    onHash('#/load-home-page', function () { loadHomePage(); ... })
+//    the function will be triggered when the hash became '#/load-home-page'
+//
+//    if we want to keep track for a specific parameter after the hash string we use
+//    onHash('#/load-page-*', function (pageNumber) { loadPage(pageNumber); ... })
+//    the function will be triggered when the hash became '#/load-page-1', '#/load-page-2', ... '#/load-page-n'
+//    and all after the string '#/load-page-' will be send as a parameter
+window.triggerHash = function (hash) {
+  // Search for a match through all saved hashes
+  for(var savedHash in window.hashes) {
+    // Getting all callback functions
+    var callbacks = window.hashes[savedHash];
+    
+    // Chech if there's a parameter hash match
+    var parameterHash   = savedHash.split('*');
+    var isParameterHash = !hash.indexOf(parameterHash[0]) && parameterHash.length == 2 && parameterHash[1] == '';
+    var parameter       = isParameterHash ? hash.replace(parameterHash[0], '') : undefined;
+    
+    // Run all callback functions only if there's a perfect match or a parameter hash match
+    if (hash == savedHash || isParameterHash) {
+      // Cache the number of all callback functions for maximum loop performance
+      var totalCallbacks = callbacks.length;
+      
+      // Run all callback functions for the specific hash key
+      for(var i=0; i<totalCallbacks; i++) {
+        callbacks[i].call(window, parameter);
+      }
+    }
+  }
+};
+
 
 // Create an alternative to jQuery onhashchange event
+// if there's a change in the page address hahs
+// well remove the hash from the address and trigger a global event with the hash key
 function checkHash() {
   if(hash = window.location.hash) {
     // Remove the hash from the address bar
     window.location.hash = '';
     
-    // Call a callback function if there's any in the Global register
-    if(typeof(callback = window.onHash[hash]) == 'function') {
-      callback.call();
-    }
+    // Trigger a global hash event
+    window.triggerHash(hash);
   }
 }
-setInterval(checkHash, 100);
+
+
+// As soon as the page loads
+// we'll strart scanning for any page address change
+window.onload = function () {
+  setInterval(checkHash, 100);
+}
 
 
 // Create a Global var to keep track of the player session
--- kit/js/newBadge.js
+++ kit/js/newBadge.js
@@ -1,6 +1,64 @@
 /* Controllers used in home.html */
 
-function newBadgeCtrl($scope) {
+// 
+function loadBadge($scope, badge) {
+  /*
+  setTimeout(function() {
+    $scope.metaData = {
+      "title"      : "2 Congratulations on "+ badge.header,
+      "description": "2 "+ badge.description,
+      "image"      : "2 "+ badge.image
+    };
+    
+    log('Change 1');
+  }, 3000);
+  */
+  
+  
+  onHash('#/new-badge/*', function (badgeNumber) {
+    log('load badge '+ badgeNumber);
+    
+    log($scope.metaData.title);
+    $scope.metaData.title = "New Page Title";
+    
+  });
+  
+  
+  // 
+  $scope.metaData = {
+    "title"      : "Congratulations on "+ badge.header,
+    "description": badge.description,
+    "image"      : badge.image
+  }
+}
+
+
+// Handles the page meta data
+function newBadgeHeaderCtrl($scope, $resource) {
+  // Load all new Badges
+  $scope.newBadges = $resource('../jsonapi/newbadges').query(function(newBadges) {
+    loadBadge($scope, newBadges[0]);
+  });
+}
+
+
+// Handles the shown popUp
+function newBadgePopUpCtrl($scope) {
+  
+  /*
+  setTimeout(function() {
+    
+    log($scope.popUp.label);
+    $scope.popUp.label = "2 Badge Information";
+    
+    document.body.click();
+    // $('.popUp .container .mainContent').click()
+    
+    log('Change 2');
+  }, 3000);
+  */
+  
+  
   // Main content class
   $scope.contentClass = "newBadge";
   
@@ -24,6 +82,11 @@ function newBadgeCtrl($scope) {
         // Manage btn visibility
         "show" : function() { return true }
       }
-    ]
+    ],
+    
+    // Execute when the popUp is been closed/hidded
+    "onClose": function() {
+      triggerHash('#/new-badge/1');
+    }
   }
 }
