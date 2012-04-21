29b82ad6b7c804368323b6df76ce2096162f892f
IvanDimanov <IvanDimanov1@gmail.com>
2012-04-21 21:40:26 +0300

    add hash_to_fn.js create multi badge handler update ng-include for all script loading

--- kit/js/commonTools.js
+++ kit/js/commonTools.js
@@ -35,13 +35,44 @@ function getFirstURLChars() {
 }
 
 
+/**
+ * Add a script tag in the page header
+ * 
+ * @param attributes - JSON Object - Hold the script tag "src" and "onload" attributes
+ */
+function addScript(attributes) {
+  var scriptTag, src;
+  
+  // Create a script tag
+  scriptTag = document.createElement('script');
+  
+  // Load the script source
+  scriptTag.setAttribute('src', attributes.src);
+  
+  // Set a on load function if needed
+  if (attributes.onload) {
+    // Set the on load function as IIFE
+    scriptTag.setAttribute('onload', '(' + attributes.onload + '())');
+  }
+  
+  // Add the script tag in the page head
+  document.getElementsByTagName('head')[0].appendChild(scriptTag);
+}
+
+
 // Clamp number in range
 function clamp(number, _lowLimit, _highLimit) {
+  var lowLimit, highLimit;
+  
+  // Secure low and high limit numbers
   lowLimit  = Math.min(_lowLimit, _highLimit);
   highLimit = Math.max(_lowLimit, _highLimit);
   
+  // Calculate  income number range
   number = number > highLimit ? highLimit : number;
   number = number < lowLimit  ? lowLimit  : number;
+  
+  // Return the in range number
   return number;
 }
 
@@ -87,9 +118,13 @@ function getUserLoggedInStatus(player) {
 }
 
 
-// Counts each element string length and when the sum of all gets the maxLength, return the clamp array
-// param: array     - the array that need to be clamped
-// param: maxLength - the maximum sum length that the clamped array elements won't exceed
+/**
+ * Counts each element string length and when the sum of all gets the maxLength, return the clamp array
+ *
+ * @param  array        array that need to be clamped
+ * @param  maxLength    maximum sum length that the clamped array elements won't exceed
+ * @return clampedArray final array with sum elements string lenght not more than maxLength
+ */
 function clampArrayByStringLength(array, maxLength) {
   // Secure income variables
   if(Object.prototype.toString.call(array) != '[object Array]' || !(maxLength *= 1)) {
@@ -115,7 +150,64 @@ function clampArrayByStringLength(array, maxLength) {
 }
 
 
-// Return a deep copy-by-value object
+/**
+ * Return a deep copy-by-value object
+ * The loop is recursive
+ * 
+ * @param  obj      the original object tha will be copied
+ * @return cloneObj the final copy-by-value object
+ */
 function clone(obj) {
-  return eval(uneval(obj));
+  var cloneObj = {},
+      tempObj  = '';
+  
+  // Chech if the income is an object or primitive value
+  if (typeof(obj) == "object") {
+    
+    // Recursive loop through all object elements
+    for (tempObj in obj) {
+      cloneObj[tempObj] = clone(obj[tempObj]);
+    }
+    
+    // Return the clone object
+    return cloneObj;
+    
+  } else {
+    
+    // Return the primitive value
+    return obj;
+  }
+}
+
+
+
+/**
+ * Add a callback function to a HTML node element event.
+ * Similar to jQuery.on() function
+ * 
+ * @param nodeElement     the HTML element that we are listening for an event
+ * @param event           event name as a string, e.q. 'click', 'hover', 'load', etc.
+ * @param callback        the function that we want to be exacuted for the specific node event
+ * @param useCaptureOrder specify the event execution order. More info on http://www.quirksmode.org/js/events_order.html
+ */
+function addEvent(nodeElement, event, callback, useCaptureOrder) {
+  // Bind the event listener for the specific browser function
+  if (nodeElement.addEventListener) {
+    // Event listener function for Webkit, Netscape, and IE engine > 8
+    
+    // Take the capture event order as a default order
+    useCaptureOrder = typeof(useCaptureOrder) != "undefined" ? useCaptureOrder : true;
+    
+    // Bind the node element event with the respective callback function in the predefined event order
+    nodeElement.addEventListener(event, callback, useCaptureOrder);
+    
+  } else if (nodeElement.attachEvent) {
+    // This event listener function is used for IE <= 8
+    // Bind the node element event with the callback function
+    nodeElement.attachEvent('on' + event, callback);
+    
+  } else {
+    // no event support
+    alert('Your browser does not support event handlers!');
+  }
 }
--- kit/js/newBadge.js
+++ kit/js/newBadge.js
@@ -1,92 +1,126 @@
 /* Controllers used in home.html */
-
-// 
-function loadBadge($scope, badge) {
-  /*
-  setTimeout(function() {
-    $scope.metaData = {
-      "title"      : "2 Congratulations on "+ badge.header,
-      "description": "2 "+ badge.description,
-      "image"      : "2 "+ badge.image
-    };
-    
-    log('Change 1');
-  }, 3000);
-  */
+(function () {
+  "use strict";
+  
+  // Shared resources between controllers and common loading badge function
+  var sharedScope = {
+    "header"  : null,
+    "popUp"   : null,
+    "resource": null,
+    "badges"  : null
+  },
+  // Keep track for the shown badges from the badges array
+  badgeNumber;
   
   
-  onHash('#/new-badge/*', function (badgeNumber) {
-    log('load badge '+ badgeNumber);
+  /**
+   * Common loading badge function
+   * Loads each badge till it shown all new badges from the /jsonapi/newbadges
+   * @param number indicate which badge from the common array is needed to be loaded
+   */
+  function loadBadge(number) {
+    var badge;
     
-    log($scope.metaData.title);
-    $scope.metaData.title = "New Page Title";
+    // Secure a badge number
+    badgeNumber = number * 1 || 1;
     
-  });
-  
-  
-  // 
-  $scope.metaData = {
-    "title"      : "Congratulations on "+ badge.header,
-    "description": badge.description,
-    "image"      : badge.image
-  }
-}
-
-
-// Handles the page meta data
-function newBadgeHeaderCtrl($scope, $resource) {
-  // Load all new Badges
-  $scope.newBadges = $resource('../jsonapi/newbadges').query(function(newBadges) {
-    loadBadge($scope, newBadges[0]);
-  });
-}
-
-
-// Handles the shown popUp
-function newBadgePopUpCtrl($scope) {
-  
-  /*
-  setTimeout(function() {
-    
-    log($scope.popUp.label);
-    $scope.popUp.label = "2 Badge Information";
-    
-    document.body.click();
-    // $('.popUp .container .mainContent').click()
-    
-    log('Change 2');
-  }, 3000);
-  */
-  
-  
-  // Main content class
-  $scope.contentClass = "newBadge";
-  
-  // Set popUp details
-  $scope.popUp = {
-    // This name is used for switching between all popUps in the common_pop_up.html include
-    "name": "newBadge",
-    
-    // General popUp display class
-    "class": "show",
-    
-    // Shown on the top-left
-    "label": "Badge Information",
-    
-    // Continue Playing btn details
-    "btns": [
-      {
-        "label": "Continue Playing",
-        "title": "Continue Playing",
+    // Check if we've load all new badges
+    if (!sharedScope.badges) {
+      
+      // Load all new Badges
+      sharedScope.badges = sharedScope.resource('../jsonapi/newbadges').query(function(newBadges) {
+        // Cache new badges
+        sharedScope.badges = newBadges;
         
-        // Manage btn visibility
-        "show" : function() { return true }
-      }
-    ],
+        // Call the loading badges function again since we have all the badges loaded
+        loadBadge(badgeNumber);
+      });
     
-    // Execute when the popUp is been closed/hidded
-    "onClose": function() {
-      triggerHash('#/new-badge/1');
+    } else {
+      
+      // Load badge regarding the income number
+      badge = sharedScope.badges[badgeNumber-1];
+      
+      // Chech if there's any not shown badges left
+      if (badge) {
+        // Loads all headed page meta data
+        sharedScope.header.metaData = {
+          "title"      : "Congratulations on your "+ badge.header,
+          "description": badge.description,
+          "image"      : badge.image
+        };
+        
+        // Loads all popUp body data
+        sharedScope.popUp.popUp = {
+          // This name is used for switching between all popUps in the common_pop_up.html include
+          "name": "newBadge",
+          
+          // General popUp display class
+          "class": "show",
+          
+          // Shown on the top-left
+          "label": "Badge Information",
+          
+          // Image attributes
+          "image": {
+            "src": badge.image,
+            "alt": badge.header
+          },
+          
+          // Left column header
+          "header": clampString(badge.header, 20),
+          
+          // Left column badge explanation
+          "explanation": clampString(badge.explanation, 190),
+          
+          // Right column description
+          "description": clampString(badge.description, 150),
+          
+          // Continue Playing btn details
+          "btns": [
+            {
+              "label": "Continue Playing",
+              "title": "Continue Playing",
+              
+              // Manage btn visibility
+              "show" : function() { return true; }
+            }
+          ],
+          
+          // Execute when the popUp is been closed/hidded
+          "onClose": function() {
+            loadBadge(++number);
+          }
+        };
+      }
     }
   }
-}
+
+
+  // Handles the page meta data
+  window.newBadgeHeaderCtrl = function ($scope, $resource) {
+    // Create an event handler to seek for any badge URL request
+    onHash('#/new-badge/*', loadBadge);
+    
+    // Share the page header scope and $resource module
+    sharedScope.header   = $scope;
+    sharedScope.resource = $resource;
+  };
+
+
+  // Handles the shown popUp
+  window.newBadgePopUpCtrl = function ($scope) {
+    // Share the popUp body scope
+    sharedScope.popUp = $scope;
+    
+    // Main content class
+    $scope.contentClass = "newBadge";
+  };
+  
+  
+  // Load the initial badge when the page is fully loaded
+  window.onload = function () {
+    loadBadge();
+  };
+  
+}())
